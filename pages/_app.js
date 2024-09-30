f081b19ac9b1fbc3d1454ff2cddeb5a088ad9da6
Om Sharma <153528804+R0GDEV@users.noreply.github.com>
2024-09-30 21:26:39 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
-
-import { Provider } from 'react-redux';
+/*import { Provider } from 'react-redux';
+import store from '../pages/store'; 
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider>
+    <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
 }
 
-export default MyApp;
+export default MyApp;*/
+import { Provider } from 'react-redux';
+import store from '../public/store'; 
+import '../styles/globals.css';
+export default function App({ Component, pageProps }) {
+  return <Provider store={store}>
+  <Component {...pageProps} />
+</Provider>
+}
